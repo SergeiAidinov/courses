10f8c82f7497fe5c21e7cd8d8e89e0d85a3e7a09
SergeiAidinov <incoming34@yandex.ru>
2024-02-07 19:18:39 +0300

    Сделал кастомный десериалайзер

--- src/main/java/ru/yandex/incoming34/structures/dto/CoursesResponce.java
+++ src/main/java/ru/yandex/incoming34/structures/dto/CoursesResponce.java
@@ -1,12 +1,16 @@
 package ru.yandex.incoming34.structures.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import ru.yandex.incoming34.serializer.CoursesResponceSrializer;
 import ru.yandex.incoming34.structures.ErrorCode;
 import ru.yandex.incoming34.structures.ErrorMessage;
 
 @AllArgsConstructor
 @Getter
+@JsonSerialize(using = CoursesResponceSrializer.class)
 public class CoursesResponce {
 
 	private final ErrorCode errorCode;
