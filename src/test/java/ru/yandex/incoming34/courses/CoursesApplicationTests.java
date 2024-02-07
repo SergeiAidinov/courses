5f5a830d637b8dccfa604e5cbe8a65c8299ce37f
SergeiAidinov <incoming34@yandex.ru>
2024-02-07 16:56:41 +0300

    Initial commit

---
+++ src/test/java/ru/yandex/incoming34/courses/CoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.incoming34.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
