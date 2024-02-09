962b0b00e1e8ba6f8b15aaf636f01215c8a103e7
SergeiAidinov <incoming34@yandex.ru>
2024-02-09 11:48:55 +0300

    Тест работает

--- src/test/java/ru/yandex/incoming34/service/DataServiceTest.java
+++ src/test/java/ru/yandex/incoming34/service/DataServiceTest.java
@@ -2,6 +2,8 @@ package ru.yandex.incoming34.service;
 
 import lombok.AllArgsConstructor;
 import org.junit.Assert;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
@@ -12,6 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.yandex.incoming34.structures.dto.ExchangeRateWithDate;
 
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -22,16 +25,35 @@ class DataServiceTest {
 
 
     private final DataService dataService;
+    private Field exchangeRatesField;
 
     DataServiceTest(@Qualifier("dataService") DataService dataService) {
         this.dataService = dataService;
     }
 
+    @BeforeAll
+    private void before() {
+        System.out.println("Before");
+        try {
+            exchangeRatesField = DataService.class.getDeclaredField("exchangeRates");
+            exchangeRatesField.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterAll
+    private void after() {
+        exchangeRatesField.setAccessible(false);
+        Assert.assertFalse(exchangeRatesField.isAccessible());
+    }
+
+
     @Test
     void addExchangeRate() {
-        System.out.println();
+        System.out.println("11");
         dataService.addExchangeRate(new ExchangeRateWithDate("USD", new BigDecimal("98.1234"), LocalDateTime.now()));
-        Assert.assertEquals(1,1);
+        Assert.assertEquals(1, 1);
     }
 
     @Test
