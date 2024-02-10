40f38c88d058b6fd40d2a581c65b6b005d9b9050
SergeiAidinov <incoming34@yandex.ru>
2024-02-10 16:20:36 +0300

    Почти готово

--- src/test/java/ru/yandex/incoming34/controller/ControllerTest.java
+++ src/test/java/ru/yandex/incoming34/controller/ControllerTest.java
@@ -11,8 +11,11 @@ import ru.yandex.incoming34.service.DataService;
 import ru.yandex.incoming34.service.ValidationService;
 import ru.yandex.incoming34.structures.Currencies;
 import ru.yandex.incoming34.structures.dto.ExchangeRate;
+import ru.yandex.incoming34.structures.dto.ExchangeRateWithDate;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {Controller.class, DataService.class, ValidationService.class})
@@ -29,12 +32,17 @@ class ControllerTest {
     void addExchangeRate() {
         RuntimeException runtimeException = Assert.assertThrows(RuntimeException.class, () -> controller.addExchangeRate(new ExchangeRate("GBP", new BigDecimal("142.3487"))));
         Assert.assertEquals(ValidationService.UNSUPPORTED_CURRENCY, runtimeException.getMessage());
-        runtimeException = Assert.assertThrows(RuntimeException.class, () -> controller.addExchangeRate(new ExchangeRate(Currencies.USD.name(), new BigDecimal("91.3qwe"))));
-        Assert.assertEquals("Character q is neither a decimal digit number, decimal point, nor \"e\" notation exponential mark.", runtimeException.getMessage());
     }
 
     @Test
     void loadData() {
+        RuntimeException runtimeException = Assert.assertThrows(RuntimeException.class, () -> controller.loadData(
+                List.of(
+                        new ExchangeRateWithDate(Currencies.USD.name(), new BigDecimal("92.4361"), LocalDateTime.parse("2023-02-01T11:00:15.963")),
+                        new ExchangeRateWithDate("GBP", new BigDecimal("137.4361"), LocalDateTime.parse("2023-02-02T10:00:15.963"))
+                )
+        ));
+        Assert.assertEquals(ValidationService.UNSUPPORTED_CURRENCY, runtimeException.getMessage());
     }
 
 }
